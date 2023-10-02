ccdea955248b6249b8afdbb7f3657f24f80d1f84
pepe <kousaku5963@gmail.com>
2023-10-02 11:50:47 +0900

    ブランチ切り替えのため一時保存

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 ﻿#include <stdio.h>
 
+int Recursive(int n) {
+	if (n <= 1) {
+		return (1);
+	}
+
+	return (n * Recursive(n - 1) - 50);
+}
+
 int main() {
+	int n = 3;
+	int result;
+
+	result = Recursive(n);
+	printf("%dの階乗 = %d\n", n, result);
 
 	return 0;
 }
