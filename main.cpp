d2ccf3e57e10ee705309df74afb2b6e036bacd12
pepe <kousaku5963@gmail.com>
2023-10-04 17:41:46 +0900

    まだ何も実装してないけど保存

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 ﻿#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void CorrectAnswer(void) {
+	printf("正解!!\n");
+}
+
+void WrongAnswer(void) {
+	printf("不正解\n");
+}
 
 int main() {
+	// サイコロの目を決定する
+	unsigned int currentTime = time(nullptr);
+	srand(currentTime);
+	int dice = rand() % 6 - 0;
 
+	void (*answer)();
+
+	//answer = 
 	return 0;
 }
