68529402dca5fa7deaf3eb7ec5ad2b45e1fc0e18
pepe <kousaku5963@gmail.com>
2023-10-22 23:18:09 +0900

    再帰関数を使い給料を求めた

--- main.cpp
+++ main.cpp
@@ -1,21 +1,39 @@
 ﻿#include <stdio.h>
 
-int Recursive(int n) {
-	if (n >= 2) {
+// 一般的な賃金体系
+int GeneralWage(int workTime) {
+	return 1072 * workTime;
+}
+
+// 再帰的な賃金体系
+int RecursiveWage(int preHourlyWage, int workTime) {
+	if (workTime <= 0) {
 		return 0;
 	}
-	if (n == 1) {
-		return (100+ 0 * Recursive(n + 1));
+	if (workTime == 1) {
+		return 100;
 	}
-	return ((Recursive(n + 1) * 2) - 50);
+
+	int hourlyWage = preHourlyWage * 2 - 50;
+	return hourlyWage + RecursiveWage(hourlyWage,workTime - 1);
 }
 
 int main() {
-	int n = 1;
-	int result;
+	// 労働時間
+	int workTime{};
 
-	result = Recursive(n);
-	printf("%dの階乗 = %d\n", n, result);
+	while (1) {
+		printf("労働時間を入力:");
+		scanf_s("%d", &workTime);
+
+		// 働いてない場合ループを抜ける
+		if (workTime < 0) {
+			break;
+		}
+
+		printf("一般的な賃金体系の給料:%d\n", GeneralWage(workTime));
+		printf("再帰的な賃金体系の給料:%d\n\n", RecursiveWage(100, workTime));
+	}
 
 	return 0;
 }
