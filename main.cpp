878f040067f64b1d49e58a70185f95bfd6b7eb50
pepe <kousaku5963@gmail.com>
2023-10-22 22:43:20 +0900

    まだできてないけど保存

--- main.cpp
+++ main.cpp
@@ -1,23 +1,40 @@
 ﻿#include <stdio.h>
+#include <windows.h>
 #include <time.h>
 #include <stdlib.h>
 
-void CorrectAnswer(void) {
-	printf("正解!!\n");
+bool IsAnswer(int diceAnswer, int selectAnswer) {
+	// 正解なら
+	if (diceAnswer == selectAnswer) {
+		return true;
+	}// 不正解なら
+	else {
+		return false;
+	}
 }
 
-void WrongAnswer(void) {
-	printf("不正解\n");
+char Answer(bool isAnswer) {
+	if (isAnswer) {
+		return printf("正解");
+	}
+	else {
+		return printf("不正解");
+	}
 }
 
 int main() {
-	// サイコロの目を決定する
-	unsigned int currentTime = time(nullptr);
-	srand(currentTime);
-	int dice = rand() % 6 - 0;
+	char (*pfunc)(bool) = Answer;
 
-	void (*answer)();
+	// 入力した数字を入れる
+	int selectNum{};
+	printf("1~6の数字を入力してください:");
+	scanf_s("%d", &selectNum);
 
-	//answer = 
+	// サイコロの目をランダムで決定する
+	srand((unsigned int)time(nullptr));
+	int dice = rand() % 6 + 1;
+
+	pfunc(dice,selectNu);
+
 	return 0;
 }
