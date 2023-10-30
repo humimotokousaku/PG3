38625a4d950c5959c56abd84de85e3ba9133a035
pepe <kousaku5963@gmail.com>
2023-10-30 11:24:04 +0900

    ラムダ式を使い前回のゲームを再現

--- main.cpp
+++ main.cpp
@@ -2,54 +2,50 @@
 #include <windows.h>
 #include <time.h>
 #include <stdlib.h>
+#include <functional>
 
-
-typedef int (*PFunc)();
-
-// サイコロの目をランダムで決定
-int RandomDice() {
-	int result = rand() % 6 + 1;
-	return result;
-}
-
-// 答え
-void Answer(PFunc p, int selectAnswer) {
-	printf("正解は...?\n");
-	// 3秒間処理を停止
-	Sleep(3000);
-
-	// 正解のサイコロ
-	int answerNum = p() % 2;
-	// 予想したサイコロ
-	int selectNum = selectAnswer % 2;
-	printf("答え%d\n", answerNum);
-	printf("予想%d\n", selectNum);
-
-	// 偶数なら丁
-	if (answerNum == 0) {
-		printf("丁!\n");
-		Sleep(1500);
-	}// 奇数なら半
-	else if(answerNum != 0){
-		printf("半!\n");
-		Sleep(1500);
-	}
-
-	// どちらも同じ数なら正解
-	if (selectNum == answerNum) {
-		printf("正解!!!\n\n");
-	}
-	else {
-		printf("不正解!!!\n\n");
-	}
-}
 
 
 int main() {
 	// サイコロの目をランダムで決定する
 	srand((unsigned int)time(nullptr));
-	// 関数ポインタ
-	PFunc pfunc = &RandomDice;
+	// 
+	std::function<int()> pfunc = [&]() {
+		int result = rand() % 6 + 1;
+		return result; 
+	};
+
+	// 結果発表
+	std::function<void(int, int)> fx = [&](int p, int selectAnswer) {
+		printf("正解は...?\n");
+		// 3秒間処理を停止
+		Sleep(3000);
+
+		// 正解のサイコロ
+		int answerNum = p % 2;
+		// 予想したサイコロ
+		int selectNum = selectAnswer % 2;
+		printf("答え%d\n", answerNum);
+		printf("予想%d\n", selectNum);
+
+		// 偶数なら丁
+		if (answerNum == 0) {
+			printf("丁!\n");
+			Sleep(1500);
+		}// 奇数なら半
+		else if (answerNum != 0) {
+			printf("半!\n");
+			Sleep(1500);
+		}
+
+		// どちらも同じ数なら正解
+		if (selectNum == answerNum) {
+			printf("正解!!!\n\n");
+		}
+		else {
+			printf("不正解!!!\n\n");
+		}
+	};
 	// 入力した予想
 	int selectAnswer;
 
@@ -60,7 +56,7 @@ int main() {
 		scanf_s("%d", &selectAnswer);
 
 		// 結果発表
-		Answer(pfunc, selectAnswer);
+		fx(pfunc(), selectAnswer);
 	}
 
 	return 0;
