56673281dceb9ff2655ebeb98558edb80de864c3
pepe <kousaku5963@gmail.com>
2023-11-06 10:42:38 +0900

    エラーがあるので修正中

--- Comparator.h
+++ Comparator.h
@@ -1,5 +1,26 @@
 #pragma once
+
+template<typename T1, typename T2>
+
 class Comparator
 {
+public:// ƒƒ“ƒo•Ï”
+	T1 num1_;
+	T2 num2_;
+
+public:// ƒƒ“ƒoŠÖ”
+	Comparator(T1 num1, T2 num2) : num1_(num1), num2_(num2) {}
+	
+	template<typename T>
+	// ’l‚Ì¬‚³‚¢‚Ù‚¤‚ğ•Ô‚·
+	T Min() {
+		if (num1_ < num2_) {
+			return static_cast<T>(num1);
+		}
+		else if (num1_ > num2_) {
+			return static_cast<T>(num2_);
+		}
+	}
+
 };
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 ﻿#include <stdio.h>
+#include "Comparator.h"
 
 int main() {
+	// 比較する数値のの組み合わせ
+	Comparator<int, int> comparator1(1, 1);
+	Comparator<int, float> comparator2(1, 1.5f);
+	Comparator<int, double> comparator3(1, 1.6);
+	Comparator<float, float> comparator4(1.5f, 1.5f);
+	Comparator<float, double> comparator5(1.5f, 1.6);
+	Comparator<double, double> comparator6(1.6, 1.6);
+
+	// それぞれの結果を表示
+	printf("<int, int>       %d", comparator1.Min<int>());
+	printf("<int, float>     %f", comparator2.Min<float>());
+	printf("<int, double>    %lf", comparator3.Min<double>());
+	printf("<float, float>   %f", comparator4.Min<float>());
+	printf("<float, double>  %lf", comparator5.Min<double>());
+	printf("<double, double> %lf", comparator6.Min<double>());
 
 	return 0;
 }
