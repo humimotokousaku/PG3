7e6737b1cc40713e619e68766c6443b046fb9ae3
pepe <kousaku5963@gmail.com>
2023-12-27 13:48:10 +0900

    stlコンテナを使用して山手線の駅名リストを作成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,56 @@
 ﻿#include <stdio.h>
+#include <list>
 
 int main() {
+	// 山手線を宣言
+	std::list<const char*> YamanoteLine = {
+	"Tokyo","Kanda","Akihabara","Okachimachi","Ueno","Uguisudani","Nippori","Tabata",
+	"Komagome","Sugamo","Otsuka","Ikebukuro","Mejiro","Takadanobaba","Shin-Okubo","Shinjuku",
+	"Yoyogi","Harajuku","Shibuya","Ebisu","Meguro","Gotanda","Osaki","Shinagawa","Tamachi","Hamatsucho",
+	"Shimbashi","Yurakucho"
+	};
+
+	printf("1970年\n");
+	for (std::list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++) {
+		printf(*itr);
+		// 文字列の最後は矢印をつけない
+		if (itr != --YamanoteLine.end()) {
+			printf("→");
+		}
+	}
+	printf("\n\n");
+
+	printf("2019年\n");
+	for (std::list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++) {
+		if (*itr == "Tabata") {
+			itr = YamanoteLine.insert(itr, "Nishi-Nippori");
+			break;
+		}
+	}
+	for (std::list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++) {
+		printf(*itr);
+		// 文字列の最後は矢印をつけない
+		if (itr != --YamanoteLine.end()) {
+			printf("→");
+		}
+	}
+	printf("\n\n");
+
+	printf("2022年\n");
+	for (std::list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++) {
+		if (*itr == "Tamachi") {
+			itr = YamanoteLine.insert(itr, "Takanawa-Gateway");
+			break;
+		}
+	}
+	for (std::list<const char*>::iterator itr = YamanoteLine.begin(); itr != YamanoteLine.end(); itr++) {
+		printf(*itr);
+		// 文字列の最後は矢印をつけない
+		if (itr != --YamanoteLine.end()) {
+			printf("→");
+		}
+	}
+	printf("\n");
 
 	return 0;
 }
