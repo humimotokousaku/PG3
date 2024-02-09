99392dc276540b34e0818c32e22a54376fcb134e
pepe <kousaku5963@gmail.com>
2024-02-10 07:52:22 +0900

    コピーと移動にかかる時間を計測

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 ﻿#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <chrono>
 
 int main() {
+    // 初期化
+    std::string a(100000, 'a');
 
-	return 0;
+    // コピーの計測
+    std::string copy{};
+    // 計測開始
+    std::chrono::system_clock::time_point startCopy = std::chrono::system_clock::now();
+    copy = a;
+    // 計測終了
+    std::chrono::system_clock::time_point endCopy = std::chrono::system_clock::now();
+    auto durationCopy = std::chrono::duration_cast<std::chrono::microseconds>(endCopy - startCopy).count();
+
+    // 移動の計測
+    std::string move{};
+    // 計測開始
+    std::chrono::system_clock::time_point startMove = std::chrono::system_clock::now();
+    move = std::move(a);
+    // 計測終了
+    std::chrono::system_clock::time_point endMove = std::chrono::system_clock::now();
+    auto durationMove = std::chrono::duration_cast<std::chrono::microseconds>(endMove - startMove).count();
+
+    // 計測結果を表示
+    std::cout << "100,000文字を移動とコピーで比較しました。" << std::endl;
+    std::cout << "コピー: " << durationCopy << "μs" << std::endl;
+    std::cout << "移動: " << durationMove << "μs" << std::endl;
+
+    return 0;
 }
